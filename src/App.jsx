97426cfd72b3948e8261515443eb9a999e2d8c2e
Forest wilson <hexaforest@gmail.com>
2024-01-23 19:37:13 -0500

    nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,34 @@ import { CreatePannels } from './lib/js/Pannels.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
-  
+  const NavRollout = () => {
+    const navOps = ["index", "home", "pics", "mp3s", "notes", "about", "settings"];
+
+    const justPickOne = navOps.map((op, idx) => (
+      <li><button id={`${op}-nBtn`} className={op} onClick={() => console.log(`${op}-nBtn`)}>
+        {op}
+      </button></li>
+    ))
+    return (
+      <ul className='nav-btns'>
+        {justPickOne}
+      </ul>
+    )
+  }
+
   return (
     <>
-      <div>
+      <div className='nav-deck'>
   
         <a href="https://burnthis.space" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <NavRollout />
+        
       </div>
 
+      <div className="card-deck">
       <h1>Grabber</h1>
-      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
