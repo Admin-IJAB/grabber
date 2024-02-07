507e156a8e00e479796f3752c3f9f84311663466
ForestW70 <hexaforest@gmail.com>
2024-02-06 22:42:35 -0500

    rolling out song view

--- src/lib/pages/AllMp3s.jsx
+++ src/lib/pages/AllMp3s.jsx
@@ -1,43 +1,94 @@
 import { Link, useParams } from "react-router-dom"
 import { sketches } from "../data/mp3-data"
 
-const loadDefault = () => {
-
-    const test = (trackList) => {
-        const listy = tracklist.map((track, i) => (
-            {
-                TrTitle: track.track,
-                length: track.length,
-                mp3Url: track.url,
-                ogFile: track.fileName,
-                daw: track.daw,
-                startD: track.date,
-                startT: track.started
-            }
-        ))
-    }
-
-    let mySongs = sketches.map((album, idx) => (
+const songView = () => {
+    let iTunes = [
         {
-            title: album.title,
-            artist: album.longArtist,
-            folderUrl: album.artLink,
-            refId: album.trackList[0].track,
-            chNum: idx
-        }
+        alTitle: "Album",
+        alArtist: "Artist",
+        alArtUrl: "URL",
+        trTitle: "Title",
+        trLength: "Length",
+        trFile: "File",
+        refId: "#",
+        trDate: "Date",
+        trTime: "StTme",
+        },
+    ];
+
+
+    sketches.map((album, i) => {
+        album.trackList.map((track, ii) => ((
+            iTunes.push({
+                alTitle: album.title,
+                alArtist: album.artist,
+                alArtUrl: album.artLink,
+                trTitle: track.track,
+                trLength: track.length,
+                trFile: track.fileName,
+                refId: ii,
+                trDate: track.date,
+                trTime: track.started,
+            })
+        )))
+    })
+
+    const songRows = iTunes.map((song, idx) => (
+            <article id={song.trTime} className="song-row" key={song.alTitle + idx}>
+                <span>{idx > 0 ? idx : "#"}</span>
+                <span>{song.alTitle}</span>
+                <span>{song.alArtist}</span>
+                <span>{song.trTitle}</span>
+                <span>{song.trLength}</span>
+                <span>{song.alArtUrl}</span>
+                <span>{song.trFile}</span>
+                <span>{song.trDate}</span>
+                <span>{song.trTime}</span>
+            </article>
     ))
 
-    const iTunes = mySongs.map((song, idx) => (
-        <article className="song-row" key={song.title}>
-            <span>{song.chNum}</span>
-            <span>{song.title}</span>
-            <span>{song.artist}</span>
-            <span>{song.folderUrl}</span>
-            <span>Listen now~</span>
-        </article>
-    ))
+    return (
+        <>
+            <p>Looking at Song View</p>
+            {songRows}
+        </>
+    )
+}
+const loadDefault = () => {
+    // {
+    //     TrTitle: track.track,
+    //     length: track.length,
+    //     mp3Url: track.url,
+    //     ogFile: track.fileName,
+    //     daw: track.daw,
+    //     startD: track.date,
+    //     startT: track.started
+    // }
 
-    return iTunes;
+    // let mySongs = sketches.map((album, idx) => (
+    //     album.trackList.map((track, iidx)) => (
+
+    //         {
+    //             title: album.title,
+    //             artist: album.longArtist,
+    //             folderUrl: album.artLink,
+    //             refId: album.trackList[0].track,
+    //             chNum: idx
+    //         }
+    //     )
+    // ))
+
+    // const iTunes = mySongs.map((song, idx) => (
+    //     <article className="song-row" key={song.title}>
+    //         <span>{song.chNum}</span>
+    //         <span>{song.title}</span>
+    //         <span>{song.artist}</span>
+    //         <span>{song.folderUrl}</span>
+    //         <span>Listen now~</span>
+    //     </article>
+    // ))
+
+    // return iTunes;
 }
 
 export const AllMp3s = () => {
@@ -56,7 +107,7 @@ export const AllMp3s = () => {
     return (
         <>
             <h2>mp3s page</h2>
-            {loadDefault()}
+            {songView()}
         </>
     )
 }
